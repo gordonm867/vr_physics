af791df0735f4f1414c68f3b388ff580164a4bb0
gordonm <9099488+gordonm867@users.noreply.github.com>
2020-07-25 17:50:17 -0400

    Update VirtualRobotApplication.java

--- Controller/src/virtual_robot/controller/VirtualRobotApplication.java
+++ Controller/src/virtual_robot/controller/VirtualRobotApplication.java
@@ -5,6 +5,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
@@ -15,6 +16,30 @@ import javafx.stage.WindowEvent;
 public class VirtualRobotApplication extends Application {
 
     private static VirtualRobotController controllerHandle;
+    private static VirtualRobotApplication instance;
+
+    public boolean right;
+    public boolean left;
+    public boolean up;
+    public boolean down;
+    public boolean a;
+    public boolean b;
+    public boolean x;
+    public boolean y;
+    public boolean q;
+    public boolean e;
+    public boolean w;
+    public boolean h;
+    public boolean g;
+    public boolean skey;
+
+    public VirtualRobotApplication() {
+         instance = this;
+    }
+
+    public static VirtualRobotApplication getInstance() {
+        return instance;
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -22,8 +47,51 @@ public class VirtualRobotApplication extends Application {
         Parent root = (BorderPane)loader.load();
         controllerHandle = loader.getController();
         primaryStage.setTitle("Virtual Robot");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.setResizable(false);
+        Scene myScene = new Scene(root);
+        myScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case I:     up      = true; break;
+                    case K:     down    = true; break;
+                    case J:     left    = true; break;
+                    case L:     right   = true; break;
+                    case A:     a       = true; break;
+                    case B:     b       = true; break;
+                    case X:     x       = true; break;
+                    case Y:     y       = true; break;
+                    case E:     e       = true; break;
+                    case Q:     q       = true; break;
+                    case S:     skey    = true; break;
+                    case W:     w       = true; break;
+                    case H:     h       = true; break;
+                    case G:     g       = true; break;
+                }
+            }
+        });
+        myScene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case I:     up      = false; break;
+                    case K:     down    = false; break;
+                    case J:     left    = false; break;
+                    case L:     right   = false; break;
+                    case A:     a       = false; break;
+                    case B:     b       = false; break;
+                    case X:     x       = false; break;
+                    case Y:     y       = false; break;
+                    case E:     e       = false; break;
+                    case Q:     q       = false; break;
+                    case S:     skey    = false; break;
+                    case W:     w       = false; break;
+                    case H:     h       = false; break;
+                    case G:     g       = false; break;
+                }
+            }
+        });
+        primaryStage.setScene(myScene);
+        primaryStage.setResizable(true);
         primaryStage.setOnShowing(new EventHandler<WindowEvent>() {
             @Override
             public void handle(WindowEvent event) {
